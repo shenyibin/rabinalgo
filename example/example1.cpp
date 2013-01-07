7945590c59413a303e2233c726a209d291e194a0
Yibin Shen <zituan@taobao.com>
2013-01-07 16:00:22 +0800

    add sha1 hash support

--- example/example1.cpp
+++ example/example1.cpp
@@ -7,27 +7,54 @@
 #include <ctype.h>	// for 'isprint'
 
 #include "rabinpoly.h"
+#include "sha1.h"
 
-#define FINGERPRINT_PT 0xbfe6b8a5bf378d83LL	
-		// value take from LBFS fingerprint.h. For detail on this, 
-     		// refer to the original rabin fingerprint paper.
+#define FINGERPRINT_PT  0xbfe6b8a5bf378d83LL
+#define BREAKMARK_VALUE 0x78
+#define MIN_CHUNK_SIZE  (2 * 1024)
+#define MAX_CHUNK_SIZE  (64 * 1024 -1)
+#define MEAN_CHUNK_SIZE MIN_CHUNK_SIZE
+
 #define BUFSIZE	1024
 
+struct chunk {
+    off_t offset;
+    size_t size;
+    unsigned char sha1sum[20];
+};
+
 char* progname;
+
+char *sha1_to_hex(const unsigned char *sha1)
+{
+    static char buffer[50];
+    static const char hex[] = "0123456789abcdef";
+    char *buf = buffer;
+    int i;
+
+    for (i = 0; i < 20; i++) {
+	unsigned int val = *sha1++;
+	*buf++ = hex[val >> 4];
+	*buf++ = hex[val & 0xf];
+    }
+    return buffer;
+}
 
 void usage() {
     fprintf(stderr, "Usage: %s <file-to-read> <sliding-window-size>\n", progname);
     fprintf(stderr, "\tExample) %s testfile 16\n", progname);
-
 }
 
-int run_task(char* filename, int windowsize) {
-
+int run_task(char* filename, int windowsize) 
+{
     int count;
     int fd;
-
+    off_t _cur_pos = 0, _last_pos = 0;
+    int min_size_suppress = 0, max_size_suppress = 0;
     u_int64_t rabinf;		
-    char buf[BUFSIZE];		// buffer to read data in
+    unsigned char buf[BUFSIZE];		// buffer to read data in
+    struct sha1_ctx c;
+    unsigned char sha1[20];
 
     // INITIALIZE RABINPOLY CLASS
     window myRabin(FINGERPRINT_PT, windowsize);
@@ -42,22 +69,44 @@ int run_task(char* filename, int windowsize) {
 	return -1;
     }
 
-    while( (count = read(fd, buf, BUFSIZE)) > 0 ) {
-	  
-	// FEED RABINPOLY CLASS BYTE-BY-BYTE
-	for (int i=0; i<count; i++) {
-	    rabinf = myRabin.slide8(buf[i]);
-	    
-	    if (isprint((int)buf[i])) {
-		printf("%c\t", buf[i]);
- 	    } else {
-		printf("0x%x\t", buf[i]);
-	    }
+    sha1_init(&c);
 
-	    printf("==>\t%lu\n", rabinf);
+    while( (count = read(fd, buf, BUFSIZE)) > 0 ) {
+	// FEED RABINPOLY CLASS BYTE-BY-BYTE
+	// count the offset in file
+	for (int i=0; i<count; i++, _cur_pos++) {
+	    rabinf = myRabin.slide8(buf[i]);
+	    size_t cs = _cur_pos - _last_pos;
+	    sha1_update(&c, &buf[i], 1); //update sha1 for each data
+
+	    if ((rabinf % MEAN_CHUNK_SIZE) == BREAKMARK_VALUE && cs < MIN_CHUNK_SIZE)
+		min_size_suppress++;
+	    else if (cs == MAX_CHUNK_SIZE)
+		max_size_suppress++;
+	    if (((rabinf % MEAN_CHUNK_SIZE) == BREAKMARK_VALUE && cs >= MIN_CHUNK_SIZE)
+		|| cs >= MAX_CHUNK_SIZE) {
+		/*reset rabin poly buf*/
+		myRabin.reset();
+		/* finish sha1 hash */
+		sha1_final(&c, sha1);
+
+		struct chunk new_chunk;
+		new_chunk.offset = _last_pos;
+		new_chunk.size = cs;
+		memcpy(new_chunk.sha1sum, sha1, 20);
+
+		/* reset cursor */
+		_last_pos = _cur_pos;
+		/* */
+		printf("%s\t%lu\n", sha1_to_hex(new_chunk.sha1sum),
+		       new_chunk.size);
+
+		/* reset sha1 digest */
+		sha1_init(&c);
+	    }
 	}
     }
-	
+    /*FIXME : we should handle last chunk here*/
     close(fd);
     return 1;
 }
